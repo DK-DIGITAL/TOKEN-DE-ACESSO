a6d226e2298f656d9df10182077e202c08596673
Saúde <116895215+DK-DIGITAL@users.noreply.github.com>
2025-04-27 11:19:41 -0300

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,7 +2,17 @@ export default function handler(req, res) {
   const clientId = process.env.CLIENT_ID;
   const redirectUri = process.env.REDIRECT_URI;
 
-  const authUrl = https://www.amazon.com/ap/oa?client_id=${clientId}&scope=cpc_advertising:campaign_management&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)};
+  console.log("CLIENT_ID: ", clientId);  
+  console.log("REDIRECT_URI: ", redirectUri);  
+
+  if (!clientId || !redirectUri) {
+    return res.status(400).json({ error: "Missing environment variables." });
+  }
+
+  // Adicionando os três scopes aprovados pela Amazon
+  const scopes = 'advertising::audiences,advertising::campaign_management,advertising::create_account';
+
+  const authUrl = https://www.amazon.com/ap/oa?client_id=${clientId}&scope=${scopes}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)};
 
   res.redirect(authUrl);
 }
